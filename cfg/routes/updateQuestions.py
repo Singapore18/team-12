1c9c8061b70d476481b1a14aedde7c5cac7672d8
bricakeld <aldricbek@gmail.com>
2018-10-12 12:48:50 +0800

    add update questions

---
+++ cfg/routes/updateQuestions.py
@@ -0,0 +1,31 @@
+import logging
+
+from flask import request, jsonify;
+
+from cfg import app;
+
+from cfg.routes import connection_manager
+
+logger = logging.getLogger(__name__)
+
+@app.route('/updatequestions', methods=['POST'])
+def updateQuestions():
+    factory = connection_manager.connection_manager()
+    connection = factory.connection
+    cursor = connection.cursor()
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    inputValue = data.get("data");
+    query = "UPDATE question SET `category`=\"{}\", `reversed`={}, `questiontext`=\"{}\", `scale`={}, `isscale`={}, `options`=\"{}\" WHERE `idquestion`={};"
+    output = {}
+    output['status'] = ""
+    for qs in inputValue:
+        print(qs)
+        try:
+            print(str.format(query, qs['category'], qs['reversed'], qs['questiontext'], qs['scale'], qs['isscale'], qs['options'], qs['questionid']))
+            cursor.execute(str.format(query, qs['category'], qs['reversed'], qs['questiontext'], qs['scale'], qs['isscale'], qs['options'], qs['questionid']))
+            output['status'] = 'OK'
+        except:
+            output['status'] = 'ERROR'
+    logging.info("My result :{}".format(output))
+    return jsonify(output);
